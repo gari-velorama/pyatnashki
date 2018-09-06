2ae6f67247c277a5950ffdf5f020b896bf11c4f1
gari-velorama <gari.velorama@yandex.ru>
2018-09-06 17:04:23 +0300

    First Project

---
+++ BoardF/BoardFTest.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 06.09.2018
+ * Time: 16:47
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+using NUnit.Framework;
+
+namespace BoardF
+{
+	[TestFixture]
+	public class BoardFTest
+	{
+/*
+		[Test]
+		public GameTest (int size)
+		{
+
+		}*/
+		[Test]
+		public void StartTest ()
+		{
+			Game game = new Game(4);
+			game.Start();
+			Assert.AreEqual (1, game.GetDigitAt (0,0));
+			
+		}
+		[Test]
+		public int PressAtTest ()
+		{
+
+		}
+		[Test]
+		public int GetDigitAtTest ()
+		{
+
+		}
+		[Test]
+		public bool SolvedTest()
+		{
+
+		}
+	}
+}
---
+++ BoardF/Game.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 06.09.2018
+ * Time: 16:39
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace BoardF
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class Game
+	{
+		int size;
+
+		public Game (int size)
+		{
+			this.size = size;
+		}
+
+		public void Start (int seed = 0)
+		{
+
+		}
+
+		public int PressAt (int x, int y)
+		{
+			return 0;
+		}
+		
+		public int GetDigitAt (int x, int y)
+		{
+			return 0;
+		}
+		
+		public bool Solved()
+		{
+			return false;
+		}
+	}
+}
